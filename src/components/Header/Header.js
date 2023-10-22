36cc0718792ec8287d87e301428caac49eb0f33f
Alec bana <anbana994@gmail.com>
2023-10-22 19:18:44 -0400

    styles updated

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,7 @@
 import './Header.scss';
 import brainLogo from '../../assets/Logo/BrainFlix-logo.svg';
 import avatar from '../../assets/Images/Mohan-muruge.jpg';
+import search from '../../assets/Icons/search.svg';
 
 function Header() {
     return (
@@ -8,8 +9,9 @@ function Header() {
             <img className='navi__logo' alt='logo for brainflix' src={brainLogo}/>
             <div className='navi__block'>
                 <div className='navi__box'>
+                <img className='navi__icon' alt='search icon' src={search}/>
                 <form className='navi__form'>
-                    <input className='navi__input'>
+                    <input className='navi__input' >
                     </input>
                 </form>
                 <img className='navi__user' alt='user profile' src={avatar}/>
