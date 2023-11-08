ee29b1ac58bd3ce075ff701a013e2b6538cb2fdf
Alec bana <anbana994@gmail.com>
2023-11-08 15:08:56 -0500

    changes for backend api

--- src/pages/Page/HomePage/HomePage.js
+++ src/pages/Page/HomePage/HomePage.js
@@ -3,50 +3,53 @@ import Header from "../../../components/Header/Header";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import VideoDetails from '../../../components/VideoDetails/VideoDetails';
-import Comments from '../../../components/Comments/Comments';
-import Videos from '../../../components/Videos/Videos';
+import VideoDetails from "../../../components/VideoDetails/VideoDetails";
+import Comments from "../../../components/Comments/Comments";
+import Videos from "../../../components/Videos/Videos";
 import Video from "../../../components/Video/Video";
 
 function HomePage() {
-    const[displayedVid, setDisplayedVid] = useState([])
-    const[videos, setVideos] = useState([])
+  const [displayedVid, setDisplayedVid] = useState([]);
+  const [videos, setVideos] = useState([]);
 
-    const filterVideos = videos.filter(video => video.id !== displayedVid.id)
-    console.log('filtered videos: ', filterVideos)
-    
-  const handleSelectVideo = (video) => {
-    setDisplayedVid(video)
-  }
-  
-
-  let apiKey = "f19305d5-5cbd-49ac-8870-e288c3efefea";
   const { id } = useParams();
+
+  let defaultVid = null;
+
+  if (videos.length > 0) {
+    defaultVid = videos[0].id;
+  }
+  let vidIdToDisplay = id ?? defaultVid;
+
+  const filterVideos = videos.filter((video) => video.id !== vidIdToDisplay);
+  const handleSelectVideo = (video) => {
+    setDisplayedVid(video);
+  };
+
   useEffect(() => {
     async function meeple() {
-      const response = await axios.get(`https://project-2-api.herokuapp.com/videos?api_key=${apiKey}`)
-      setVideos(response.data)
+      const response = await axios.get(`http://localhost:8080/videos`);
+      setVideos(response.data);
       if (response.data.length > 0) {
         const firstVid = response.data[0].id;
-        const responseFirstVid = await axios.get(`https://project-2-api.herokuapp.com/videos/${firstVid}?api_key=${apiKey}`)
+        const responseFirstVid = await axios.get(
+          `http://localhost:8080/videos/${firstVid}`
+        );
         setDisplayedVid(responseFirstVid.data);
       }
     }
     meeple();
-  },[])
+  }, []);
 
   useEffect(() => {
-    async function pullVideoById() {
-        console.log('andrea')
-      if(id && id !== displayedVid.id){
-    const response =await axios.get(`https://project-2-api.herokuapp.com/videos/${id}?api_key=${apiKey}`)
-        
-     setDisplayedVid(response.data)
-        
-       
-      }
+    if (id) {
+      const pullVideoById = async () => {
+        const response = await axios.get(`http://localhost:8080/videos/${id}`);
+        setDisplayedVid(response.data);
+        console.log('fix' ,response.data)
+      };
+      pullVideoById();
     }
-    pullVideoById();
   }, [id]);
 
   return (
@@ -59,7 +62,7 @@ function HomePage() {
             <VideoDetails videoInfo={displayedVid} />
             <Comments comments={displayedVid?.comments || []} />
           </section>
-          <Videos videos={filterVideos} onSelectVideo={handleSelectVideo}/>
+          <Videos videos={filterVideos} onSelectVideo={handleSelectVideo} />
         </section>
       </main>
     </>
